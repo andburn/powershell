effab97f5c9f44ead7fde1d1381542586a3421b1
Andrew Burnett <andrew@andburn.info>
2016-08-28 15:16:27 +0100

    Add 64-bit python alias

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,3 +1,4 @@
 $ScriptPath = Split-Path $Profile | Join-Path -ChildPath 'Scripts'
 New-Alias NUnit $ScriptPath\Invoke-NUnitOnProject.ps1
 New-Alias OpenCover $ScriptPath\Invoke-OpenCoverOnProject.ps1
+New-Alias python64 $env:Dev\Python\python.exe

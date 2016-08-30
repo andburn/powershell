b7785cb6f8927a999977e5fe8474fd0ba1040e41
Andrew Burnett <andrew@andburn.info>
2016-08-30 16:44:34 +0100

    Add git commit with date

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -3,5 +3,7 @@ $ScriptPath = Split-Path $Profile | Join-Path -ChildPath 'Scripts'
 New-Alias NUnit $ScriptPath\Invoke-NUnitOnProject.ps1
 New-Alias OpenCover $ScriptPath\Invoke-OpenCoverOnProject.ps1
 New-Alias BuildProj $ScriptPath\Invoke-MSBuildOnProject.ps1
+# Git
+New-Alias CommitDate $ScriptPath\Git-CommitWithDate.ps1
 # Misc
 New-Alias python64 $env:Dev\Python\python.exe
---
+++ Scripts/Git-CommitWithDate.ps1
@@ -0,0 +1,39 @@
+#Requires -Version 3.0
+
+Param(
+	[string]$Message,
+	[string]$Date,
+	[string]$Time	
+)
+
+Function GetRandomTime {
+	$hour = Get-Random -Minimum 9 -Maximum 24
+	$mins = Get-Random -Maximum 60
+	$secs = Get-Random -Maximum 60
+	Return "{0}:{1}:{2}" -f $hour.ToString("D2"), $mins.ToString("D2"), $secs.ToString("D2")
+}
+
+Function GetUTCOffset {
+	Return Get-Date -UFormat "%Z00"
+}
+
+if (-not $Message) {
+	Write-Host -ForegroundColor Red "A commit message is required"
+    Return
+}
+
+$CommitDate = Get-Date -UFormat "%Y-%m-%d %T %Z00"
+
+if ($Date) {
+	$CommitDate = $Date
+	if ($Time) {
+		$CommitDate += " $Time"
+	} else {
+		$CommitDate += " $(GetRandomTime)"
+	}
+	$CommitDate += " $(GetUTCOffset)"
+}
+
+$env:GIT_COMMITTER_DATE="`"$CommitDate`""
+git commit --date="`"$CommitDate`"" -m "`"$Message`""
+

737d9a323c4b12cd31a146b810057caf35bc8b8c
Andrew Burnett <andrew@andburn.info>
2017-05-08 22:39:55 +0100

    Delete extract path if it exists

--- Scripts/Get-LatestDeckTrackerRelease.ps1
+++ Scripts/Get-LatestDeckTrackerRelease.ps1
@@ -6,6 +6,10 @@ Param(
 
 . "$PSScriptRoot\Utils-GitHub.ps1"
 
-GetLatestRelease $RootDir "HearthSim" "Hearthstone-Deck-Tracker"
 $ExtractPath = Join-Path -Path $RootDir -ChildPath "Hearthstone Deck Tracker"
+If (test-path $ExtractPath)
+{
+	Remove-Item -Recurse -Force $ExtractPath
+}
+GetLatestRelease $RootDir "HearthSim" "Hearthstone-Deck-Tracker"
 Rename-Item -NewName "$ExtractPath\HearthstoneDeckTracker.exe" "$ExtractPath\Hearthstone Deck Tracker.exe"

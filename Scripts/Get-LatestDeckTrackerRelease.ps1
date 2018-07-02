abe7bdee5b73b7f0f5b4b86a185228c1659508bf
Andrew Burnett <andrew@andburn.info>
2018-07-02 10:16:06 +0100

    Fix GitHub relase downloader

--- Scripts/Get-LatestDeckTrackerRelease.ps1
+++ Scripts/Get-LatestDeckTrackerRelease.ps1
@@ -6,10 +6,11 @@ Param(
 
 . "$PSScriptRoot\Utils-GitHub.ps1"
 
-$ExtractPath = Join-Path -Path $RootDir -ChildPath "Hearthstone Deck Tracker"
+$Dir = Resolve-Path $RootDir
+$ExtractPath = Join-Path -Path $Dir -ChildPath "Hearthstone Deck Tracker"
 If (test-path $ExtractPath)
 {
 	Remove-Item -Recurse -Force $ExtractPath
 }
-GetLatestRelease $RootDir "HearthSim" "Hearthstone-Deck-Tracker"
+GetLatestRelease $Dir "HearthSim" "Hearthstone-Deck-Tracker" -Scrape
 Rename-Item -NewName "$ExtractPath\HearthstoneDeckTracker.exe" "$ExtractPath\Hearthstone Deck Tracker.exe"
--- Scripts/Utils-GitHub.ps1
+++ Scripts/Utils-GitHub.ps1
@@ -24,26 +24,27 @@ Param(
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 	$ZipName = "$($Repo)_latest.zip"
+	$Dir = Resolve-Path $RootDir
 
 	If ($Scrape) {
 		# scrape the html (avoids rate limit in cloud)
-		$SavePath = Join-Path -Path $RootDir -ChildPath $ZipName
-		$html = Invoke-WebRequest -Uri "https://github.com/$Owner/$Repo/releases/latest"
-		$url = ($html.ParsedHtml.getElementsByTagName('a') | Where { $_.href -like '*releases/download*' }).href.Replace('about:', 'https://github.com')
-		DownloadZipAndExtract $RootDir $SavePath $url
+		$SavePath = Join-Path -Path $Dir -ChildPath $ZipName
+		$html = Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/$Owner/$Repo/releases/latest"
+		$url = ($html.Links | Where { $_.href -like '*releases/download*' }).href
+		DownloadZipAndExtract $Dir $SavePath "https://github.com$url"
 	} else {
 		# use github api
-		$Json = Invoke-WebRequest -Uri "https://api.github.com/repos/$Owner/$Repo/releases/latest"
+		$Json = (Invoke-WebRequest -UseBasicParsing -Uri "https://api.github.com/repos/$Owner/$Repo/releases/latest").Content
 
 		# get the name of the asset (assuming only one and its a zip)
 		If ($Json -match '"name"\s*:\s*"([^"]+?\.zip)"') {
 			$ZipName = $matches[1]
 		}
-		$SavePath = Join-Path -Path $RootDir -ChildPath $ZipName
+		$SavePath = Join-Path -Path $Dir -ChildPath $ZipName
 
 		# get the location of the asset, download and extract
 		If ($Json -match '"browser_download_url"\s*:\s*"(http[^"]+?)"') {
-			DownloadZipAndExtract $RootDir $SavePath $matches[1]
+			DownloadZipAndExtract $Dir $SavePath $matches[1]
 		}
 	}
 }
